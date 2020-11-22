19d2ba4b2a9a891eb4d8472f226ee2cd345f0683
Francieli Viane <francieli.viane@ev-box.com>
2020-11-22 18:25:08 +0100

    Spring boot - initial configuration

---
+++ src/main/java/kalah/game/KalahApplication.java
@@ -0,0 +1,11 @@
+package kalah.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KalahApplication.class, args);
+    }
+}
