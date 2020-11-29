4f095fbd2314bb63f09ac1525752818585d78b76
Francieli Viane <francieli.viane@ev-box.com>
2020-11-29 17:51:41 +0100

    Dinamically sets pits and kalah amount on the board

--- client/src/components/Board.vue
+++ client/src/components/Board.vue
@@ -1,39 +1,130 @@
 <template>
   <div class="board">
     <div class="section endsection">
-      <div class="pit" id="mb">
+      <div class="pit" id="north-side-kalah">
+        <p class="pit-font kalah-seeds"> {{ northOrientationKalah.amountOfSeeds }}</p>
       </div>
     </div>
     <div class="section midsection">
       <div class="midrow topmid">
-        <div class="pit" id="pt1">
+        <div class="pit" v-for="pit in northSideOrientationPits" :key="pit.index">
+          <p class="pit-seeds pit-font">{{ pit.amountOfSeeds }}</p>
         </div>
-        <div class="pit" id="pt2">
-        </div>
-        <div class="pit" id="pt3"></div>
-        <div class="pit" id="pt4"></div>
-        <div class="pit" id="pt5"></div>
-        <div class="pit" id="pt6"></div>
       </div>
       <div class="midrow botmid">
-        <div class="pit" id="pb6"></div>
-        <div class="pit" id="pb5"></div>
-        <div class="pit" id="pb4"></div>
-        <div class="pit" id="pb3"></div>
-        <div class="pit" id="pb2"></div>
-        <div class="pit" id="pb1"></div>
+        <div class="pit" v-for="pit in southSideOrientationPits" :key="pit.index">
+          <p class="pit-seeds pit-font">{{ pit.amountOfSeeds }}</p>
+        </div>
       </div>
     </div>
     <div class="section endsection">
-      <div class="pit" id="mt"></div>
+      <div class="pit" id="mt">
+        <p class="pit-font kalah-seeds"> {{ southOrientationKalah.amountOfSeeds }}</p>
+      </div>
     </div>
   </div>
-
-
 </template>
 
 <script>
 export default {
   name: 'Board',
+  data() {
+    return {
+      "pits": [
+        {
+          "pitType": "REGULAR",
+          "index": 0,
+          "amountOfSeeds": 4
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 1,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 2,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 3,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 4,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 5,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "KALAH",
+          "index": 6,
+          "amountOfSeeds": 0
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 7,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 8,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 9,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 10,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 11,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "REGULAR",
+          "index": 12,
+          "amountOfSeeds": 6
+        },
+        {
+          "pitType": "KALAH",
+          "index": 13,
+          "amountOfSeeds": 2
+        }
+      ]
+    }
+  },
+  computed: {
+    northSideOrientationPits() {
+      return this.pits.filter(pit => pit.index > 6 && pit.index !== 13)
+          .sort(function (a, b) {
+            return a.index - b.index
+          });
+    },
+
+    northOrientationKalah() {
+      return this.pits.filter(pit => pit.index === 13)[0];
+    },
+
+    southOrientationKalah() {
+      return this.pits.filter(pit => pit.index === 6)[0];
+    },
+    southSideOrientationPits() {
+      return this.pits.filter(pit => pit.index <= 5)
+          .sort(function (a, b) {
+            return a.index - b.index
+          });
+    }
+  }
 }
 </script>
